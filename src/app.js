fdf78986a9a6040d3d3e070671374631316b44c5
Gustavo <gustavorodrigues.grm@gmail.com>
2022-11-01 19:58:04 -0300

    Corrigindo pequenos detalhes.

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Erro de conexão"));
 db.once("open", () => {
-  console.log("conexão com o banco feita com sucesso");
+  console.log("conexão com o banco de dados feita com sucesso");
 });
 
 const app = express();
